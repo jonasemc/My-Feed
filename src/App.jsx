fe4a74232f1387f522ced120b496400707fc65b2
Jonas <jonasmirandaa21@gmail.com>
2023-01-23 11:07:48 -0300

    feat: create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { Header } from "./components/Header";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
 function App() {
-  return <h1>teste</h1>;
+  return (
+    <div>
+      <Header></Header>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <h2>Jonas Miranda</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nobis
+            totam rem error impedit eos temporibus dolore at repellendus quasi
+            velit, repellat qui harum maiores earum fugiat est accusantium
+            illum.
+          </p>
+          <h2>Rodrigo Pacheco</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nobis
+            totam rem error impedit eos temporibus dolore at repellendus quasi
+            velit, repellat qui harum maiores earum fugiat est accusantium
+            illum.
+          </p>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
