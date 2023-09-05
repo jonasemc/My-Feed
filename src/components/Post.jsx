cf33424dc818ebfae3b19a0e24d94fee0616c261
Jonas <jonasmirandaa21@gmail.com>
2023-09-05 17:20:03 -0300

    feat: create comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,6 +34,13 @@ export function Post() {
           <a href="">#rocketseat </a>
         </p>
       </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentario"></textarea>
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
