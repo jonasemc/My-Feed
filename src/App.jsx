b8cc149f3ea7ab2ff23580e1f50a738c9e7b9961
Jonas <jonasmirandaa21@gmail.com>
2023-09-19 19:26:00 -0300

    refactor: update props logic for post

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,45 @@ import { Post } from "./components/Post";
 import styles from "./App.module.css";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/jonasemc.png",
+      name: "Jonas Miranda",
+      role: "Frontend Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-09-19 10:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/rodrigopacheco100.png",
+      name: "Rodrigo Pacheco",
+      role: "FullStack Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "paragraph", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-09-08 20:00:00"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -11,8 +50,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post></Post>
-          <Post></Post>
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
