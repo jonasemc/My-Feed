80df932406bd80cb5781aff02f7db698d6366a39
Jonas <jonasmirandaa21@gmail.com>
2023-09-19 17:52:38 -0300

    refactor: create avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/rodrigopacheco100.png"
-          />
+          <Avatar src="https://github.com/rodrigopacheco100.png" />
           <div className={styles.authorInfo}>
             <strong>Rodrigo Pacheco</strong>
             <span>FullStack Developer</span>
