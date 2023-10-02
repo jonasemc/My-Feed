3103fd5f7496851f184aa26521411b99052974ff
Jonas <jonasmirandaa21@gmail.com>
2023-10-02 11:00:21 -0300

    feat: create function to remove comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,10 @@ import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/jonasemc.png" />
@@ -18,7 +21,7 @@ export function Comment({ content }) {
                 Cerca de 1h atras
               </time>
             </div>
-            <button title="Deletar comentario">
+            <button onClick={handleDeleteComment} title="Deletar comentario">
               <Trash size={24} />
             </button>
           </header>
